98c1d0e1adc03043433b01f1027931513800ad4b
vladherasymenko <93705994+vladherasymenko@users.noreply.github.com>
2022-01-19 08:49:03 +0100

    Add files via upload

--- unit_tests.py
+++ unit_tests.py
@@ -20,11 +20,15 @@ warnings.filterwarnings("ignore", category=UserWarning)
 X = pd.read_csv("restored_train.csv")
 y = pd.read_csv("training_set_labels.csv").drop(['respondent_id'], axis=1)
 
+X_to_predict = pd.read_csv("restored_test.csv")
+
+resp_id = pd.read_csv("test_set_features.csv")['respondent_id']
+
 # model
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=123)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.001, random_state=321)
 regressor = MultiOutputRegressor(HistGradientBoostingRegressor(scoring="roc_auc",
-                                                               max_iter=50,
-                                                               l2_regularization=100,
+                                                               max_iter=100,
+                                                               l2_regularization=2000,
                                                                max_leaf_nodes=105,
                                                                learning_rate=0.1,
                                                                min_samples_leaf=1))
@@ -37,10 +41,31 @@ Y_predict_test[Y_predict_test < 0] = 0  # y є [0, 1]
 Y_predict_test[Y_predict_test > 1] = 1
 prec_test = roc_auc_score(y_test, Y_predict_test)
 
+print("Précision test :", prec_test)
+
+Y_predict_train = regressor.predict(X_train)
+Y_predict_train[Y_predict_train < 0] = 0  # y є [0, 1]
+Y_predict_train[Y_predict_train > 1] = 1
+prec2 = roc_auc_score(y_train, Y_predict_train)
+print("Précision train :", prec2)
+
 Y_predict_tous = regressor.predict(X)
 Y_predict_tous[Y_predict_tous < 0] = 0  # y є [0, 1]
 Y_predict_tous[Y_predict_tous > 1] = 1
 
+Y_predict = regressor.predict(X_to_predict)
+# y є [0, 1]
+Y_predict[Y_predict < 0] = 0
+Y_predict[Y_predict > 1] = 1
+result = pd.DataFrame(Y_predict)
+result["respondent_id"] = resp_id
+result = result.rename(columns={0: "h1n1_vaccine", 1: "seasonal_vaccine"})
+cols = ["respondent_id", "h1n1_vaccine", "seasonal_vaccine"]
+result = result[cols]
+result = result.astype({"respondent_id": int})
+result.to_csv("model.csv", index=False)
+
+"""
 class DonneesTestCase(unittest.TestCase):
     def setUp(self):
         self.X = np.array(X)
@@ -85,3 +110,4 @@ class PredictionTestCase(unittest.TestCase):
     # La précision acceptable est 65% ou mieux
     def test_precision_acceptable(self):
         self.assertTrue(self.precision >= 0.65)
+"""
